1e4b49a19fd6483aa9a7d4cf2930a7138fa5908c
RawRaw5 <94robertwhite@gmail.com>
2022-03-09 21:03:03 -0800

    Adding img src

--- views/home.jsx
+++ views/home.jsx
@@ -5,11 +5,17 @@ function home () {
     return (
       <Def>
           <main>
-              <h1>HOME</h1>
-              <a href="/places">
+            <h1>HOME</h1>
+            <div>
+              <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+              <div>
+                Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+              </div>
+            </div>
+            <a href="/places">
                 <button className="btn-primary">Places Page</button>
-              </a>
-          </main>
+            </a>
+        </main>
       </Def>
     )
   }
